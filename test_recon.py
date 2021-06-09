329d5e1f53599d7086f7a3655e8ab8f6b4e2006b
Vincent Du <vincent.hanxiao.du@gmail.com>
2021-06-09 20:40:56 +0800

    Add creating DATASETS_DIR for test_recon.py

--- test_recon.py
+++ test_recon.py
@@ -6,7 +6,10 @@ import h5py
 if __name__ == "__main__":
     DATA_FILE_NAME = "CelebA.h5"
     DATA_FILE_PATH = os.path.join(DATASETS_DIR, DATA_FILE_NAME)
-
+    
+    # Create DATASETS_DIR if it does not exist.
+    if not os.path.exists(DATASETS_DIR):
+        os.makedirs(DATASETS_DIR)
     # Download the data file if the file does not exist.
     if not os.path.exists(DATA_FILE_PATH):
         download_CelebA(dir=DATASETS_DIR, filename=DATA_FILE_NAME)
